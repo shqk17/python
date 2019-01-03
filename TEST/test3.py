4df69eb03d4a93a5c97dafea3c62bdd888743095
fengshuhao <fengshuhao@rybbaby.com>
2019-01-03 14:14:52 +0800

    sql

--- TEST/test3.py
+++ TEST/test3.py
@@ -1,8 +1,10 @@
-
-s = "UPDATE tss_member_package set isStatisticRemediationClassHour=isStatisticRemediationClassHour + %d , notStatisticRemediationClassHour=notStatisticRemediationClassHour - %d where id ='%s';"
-set={'2c92828665aa68940165b2e014283f3f':1,
-'2c92828665aa68940165b2dfcf823667':2,
-'2c92828665aa68940165b2e01e5840e6':2,
-'2c92828665aa68940165b2e01edc40fb':2}
-for i,j in set.items():
-    print(s%(j,j,i))
+s = "UPDATE tss_member_package set isStatisticRemediationClassHour=isStatisticRemediationClassHour " \
+    "+ %d , notStatisticRemediationClassHour=notStatisticRemediationClassHour - %d where id ='%s';"
+set = {
+    '2c92828665aa68940165b2e014283f3f': 1,
+    '2c92828665aa68940165b2dfcf823667': 2,
+    '2c92828665aa68940165b2e01e5840e6': 2,
+    '2c92828665aa68940165b2e01edc40fb': 2
+}
+for i, j in set.items():
+    print(s % (j, j, i))
--- mysqlopt.py
+++ mysqlopt.py
@@ -4,19 +4,28 @@ db = pymysql.connect(host='192.168.0.5',
                      port=3306,
                      user='root',
                      passwd='asdfg_qwert@',
-                     db='tss',
-                     charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+                     db='tss_new',
+                     charset='utf8'
+                     # , cursorclass=pymysql.cursors.DictCursor
+                     )
 cursor = db.cursor()
 
-sql = "SELECT * from tss_member_add_subtract_record " \
-      "where memberId ='2c92828665aa68940165b2e014073f3c'and " \
-      "tss_member_add_subtract_record.addSubtractType = 2 and " \
-      "tss_member_add_subtract_record.classHourType=3 and " \
-      "createTime>='2018-12-08 19:31:51'"
-data = ('13512345678',)
+sql = "SELECT a.id from tss_member_return_premium_history a " \
+      "lEFT JOIN sys_admin_user b on a.adminUserId = b.id " \
+      "LEFT JOIN sys_school c on c.id =b.schoolId " \
+      "where c.schoolName is null"
+
 cursor.execute(sql)
+i = 1
 for row in cursor.fetchall():
-    print(row)
-print('共查找出', cursor.rowcount, '条数据')
+    print(row[0])
+    sql2 = "delete from tss_member_return_premium_history where id = '%s'" % (row[0])
+    print(sql2)
+    cursor.execute(sql2)
+    i = i + 1
+db.commit()
+print('共查找出', i, '条数据')
 cursor.close()
 db.close()
+print('请输入任意键结束：')
+input()
